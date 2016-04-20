eaff2f5284f57c3a2cbd6b347296d1c2c35867e5
YosefRosenberg <Yosef.E.Rosenberg@gmail.com>
2016-04-19 17:18:25 -0700

    Initial Commit

---
+++ Picster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picster
+//
+//  Created by Yosef Rosenberg on 4/19/16.
+//  Copyright © 2016 Yosef Rosenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
